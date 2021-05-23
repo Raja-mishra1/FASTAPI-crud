1199f5f7562f8dfc27453f2dd07149590d9bc057
Mishra-raja <rajamishra0702@gmail.com>
2021-05-23 23:13:46 +0530

    [ADD]: added notes post script

---
+++ src/app/api/notes.py
@@ -0,0 +1,18 @@
+from fastapi import APIRouter, HTTPException
+
+from app.api import crud
+from app.api.models import NoteDB, NoteSchema
+
+router = APIRouter()
+
+    
+@router.post("/", response_model=NoteDB, status_code=201)
+async def create_note(payload:NoteSchema):
+    note_id = await crud.post(payload)
+    
+    response_object = {
+        "id": note_id,
+        "title": payload.title,
+        'description': payload.description,
+    }
+    return  response_object
