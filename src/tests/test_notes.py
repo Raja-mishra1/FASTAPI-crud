b0ae894320cf983418ea30496e852ded17cd1b1a
Mishra-raja <rajamishra0702@gmail.com>
2021-05-23 23:38:42 +0530

    [ADD]: added failing tests for get request

--- src/tests/test_notes.py
+++ src/tests/test_notes.py
@@ -15,7 +15,28 @@ def test_create_note(test_app, monkeypatch):
 
     assert response.status_code == 201
     assert response.json() == test_response_payload
-
+    
+def test_read_note(test_app, monkeypatch):
+    test_data = {"id": 1,"title": "something", "description": "something else"}
+    
+    async def mock_get(id):
+        return test_data
+    monkeypatch.setattr(crud,"get",mock_get)
+    
+    response = test_app.get("/notes/1")
+    assert response.status_code == 200
+    assert response.json() == test_data
+    
+def test_read_note(test_app, monkeypatch):
+    async def mock_get(id):
+        return None
+    
+    monkeypatch.setattr(crud, "get", mock_get)
+    
+    response = test_app.get("/notes/99")
+    assert response.status_code == 404
+    assert response.json()["detail"] ==  "Note not found"
+ 
 
 def test_create_note_invalid_json(test_app):
     response = test_app.post("/notes/", data=json.dumps({"title": "something"}))
