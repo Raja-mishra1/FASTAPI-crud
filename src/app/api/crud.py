207d72fe21d102f4398f9788579ad0ae7b0f594e
Mishra-raja <rajamishra0702@gmail.com>
2021-05-23 23:46:34 +0530

    [ADD]: added get endpoints for notes

---
+++ src/app/api/crud.py
@@ -0,0 +1,15 @@
+from app.api.models import NoteSchema
+from app.db import notes, database
+
+
+async def post(payload:NoteSchema):
+    query =   notes.insert().values(title = payload.title,description = payload.description)
+    return await database.execute(query=query)
+
+async def get(id: int):
+    query = notes.select().where(id == notes.c.id)
+    return await database.fetch_one(query=query)
+
+async def get_all():
+    query = notes.select()
+    return await database.fetch_all(query=query)
