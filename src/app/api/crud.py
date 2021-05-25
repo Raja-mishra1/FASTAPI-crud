2d85a47aebf7fc0fa214e097feeb8cac7aac5195
Mishra-raja <rajamishra0702@gmail.com>
2021-05-25 23:25:47 +0530

    [ADD]: added crud module for notes app

--- src/app/api/crud.py
+++ src/app/api/crud.py
@@ -13,3 +13,13 @@ async def get(id: int):
 async def get_all():
     query = notes.select()
     return await database.fetch_all(query=query)
+
+async def put(id: int, payload: NoteSchema):
+    query = (
+        notes
+        .update()
+        .where(id == notes.c.id)
+        .values(title=payload.title, description=payload.description)
+        .returning(notes.c.id)
+    )
+    return await database.execute(query=query)
